53be5defedef3822f5975422bcb35878f896c787
haunguyenphuc1110 <42568189+haunguyenphuc1110@users.noreply.github.com>
2020-06-07 14:00:47 +0700

    Project setup

--- app/src/main/java/com/example/mvvmsampleapp/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/example/mvvmsampleapp/ui/auth/SignupActivity.kt
@@ -1,12 +1,13 @@
-package com.example.mvvmsampleapp
+package com.example.mvvmsampleapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmsampleapp.R
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
